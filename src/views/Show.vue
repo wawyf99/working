81d6ad16c9669ac65dee0cfda18097141c287a58
张承 <star.cheung@live.cn>
2018-09-20 16:22:34 +0800

    3

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="shows">
+  <div id="show">
     <div class="show">
       <div class="ui-show-step1" ref="go1">{{now}}</div>
       <div class="ui-show-step2" ref="go2">"<span>亦柔</span>"邀请你加入了群聊，群聊参与人还有: 这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德哈卡烧烤、这是阿是会撒娇德</div>
@@ -140,38 +140,39 @@
     },
     mounted(){
 
-      var overscroll = function(el) {
-        console.log(el);
-        el.addEventListener('touchstart', function() {
-          var top = el.scrollTop
-            , totalScroll = el.scrollHeight
-            , currentScroll = top + el.offsetHeight;
-          //If we're at the top or the bottom of the containers
-          //scroll, push up or down one pixel.
-          //
-          //this prevents the scroll from "passing through" to
-          //the body.
-          if(top === 0) {
-            el.scrollTop = 1;
-          } else if(currentScroll === totalScroll) {
-            el.scrollTop = top - 1;
-          }
-        });
-        el.addEventListener('touchmove', function(evt) {
-          //if the content is actually scrollable, i.e. the content is long enough
-          //that scrolling can occur
-          if(el.offsetHeight < el.scrollHeight)
-            evt._isScroller = true;
-        });
-      }
-      overscroll(document.getElementById('app'));
-      document.body.addEventListener('touchmove', function(evt) {
-        //In this case, the default behavior is scrolling the body, which
-        //would result in an overflow.  Since we don't want that, we preventDefault.
-        if(!evt._isScroller) {
-          evt.preventDefault();
+      var startX = 0, startY = 0;
+
+      function touchStart(e) {
+        try {
+          var touch = e.touches[0],
+            x = Number(touch.pageX),
+            y = Number(touch.pageY);
+          startX = x;
+          startY = y;
+        } catch (e) {
+          alert(e);
         }
-      });
+      }
+
+      document.addEventListener('touchstart', touchStart);
+      var ele = document.getElementById('show');
+      ele.ontouchmove = function (e) {
+        var point = e.touches[0],
+          eleTop = ele.scrollTop,
+          eleScrollHeight = ele.scrollHeight,
+          eleOffsetHeight = ele.offsetHeight,
+          eleTouchBottom = eleScrollHeight - eleOffsetHeight;
+        if (eleTop === 0) {
+          if (point.clientY > startY) {
+            e.preventDefault();
+          }
+        }
+        else if (eleTop === eleTouchBottom) {
+          if (point.clientY < startY) {
+            e.preventDefault()
+          }
+        }
+      };
 
     }
   }
