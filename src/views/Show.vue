11181d015aba468bb25af7381f8b843b716e18e4
张承 <star.cheung@live.cn>
2018-09-20 12:12:15 +0800

    3

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -70,39 +70,37 @@
       this.getNowTime();
       this.go();
 
-      var startX = 0, startY = 0;
-
-      function touchStart(e) {
-        try {
-          var touch = e.touches[0],
-            x = Number(touch.pageX),
-            y = Number(touch.pageY);
-          startX = x;
-          startY = y;
-        } catch (e) {
-          alert(e);
-        }
+      var overscroll = function(el) {
+        el.addEventListener('touchstart', function() {
+          var top = el.scrollTop
+            , totalScroll = el.scrollHeight
+            , currentScroll = top + el.offsetHeight;
+          //If we're at the top or the bottom of the containers
+          //scroll, push up or down one pixel.
+          //
+          //this prevents the scroll from "passing through" to
+          //the body.
+          if(top === 0) {
+            el.scrollTop = 1;
+          } else if(currentScroll === totalScroll) {
+            el.scrollTop = top - 1;
+          }
+        });
+        el.addEventListener('touchmove', function(evt) {
+          //if the content is actually scrollable, i.e. the content is long enough
+          //that scrolling can occur
+          if(el.offsetHeight < el.scrollHeight)
+            evt._isScroller = true;
+        });
       }
-
-      document.addEventListener('touchstart', touchStart);
-      var ele = document.querySelector('#app');
-      ele.ontouchmove = function (e) {
-        var point = e.touches[0],
-          eleTop = ele.scrollTop,
-          eleScrollHeight = ele.scrollHeight,
-          eleOffsetHeight = ele.offsetHeight,
-          eleTouchBottom = eleScrollHeight - eleOffsetHeight;
-        if (eleTop === 0) {
-          if (point.clientY > startY) {
-            e.preventDefault();
-          }
+      overscroll(document.querySelector('.scrollEle'));
+      document.body.addEventListener('touchmove', function(evt) {
+        //In this case, the default behavior is scrolling the body, which
+        //would result in an overflow.  Since we don't want that, we preventDefault.
+        if(!evt._isScroller) {
+          evt.preventDefault();
         }
-        else if (eleTop === eleTouchBottom) {
-          if (point.clientY < startY) {
-            e.preventDefault()
-          }
-        }
-      };
+      });
 
     },
     methods:{
