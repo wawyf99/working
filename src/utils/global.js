a51c75ea818896c886869d4f37751a553c0656b8
张承 <star.cheung@live.cn>
2018-09-20 20:27:30 +0800

    3

--- src/utils/global.js
+++ src/utils/global.js
@@ -2,7 +2,11 @@
  * 全局接口请求地址类
  */
 //本地环境
-global.baseUrl = "http://localhost:3030";
+//global.baseUrl = "http://localhost:3030";
+//正式环境
+global.baseUrl = "http://working.api.rzzc.ltd";
+
+
 
 //微信分享IOS链接处理;
 global.appEntryUrl = '';
