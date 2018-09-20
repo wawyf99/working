df56e28b52cc0938f578d9d037fbab28711ef41c
张承 <star.cheung@live.cn>
2018-09-20 17:03:31 +0800

    返回监听

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -26,10 +26,21 @@
     },
     created() {
 
-      window.addEventListener("popstate", function(e) {
-// pushHistory();
-        alert("我监听到了浏览器的返回按钮事件啦");//根据自己的需求实现自己的功能
-      }, false);
+      if(window.history && window.history.pushState) {
+        $(window).on('popstate', function() {
+          var hashLocation = location.hash;
+          var hashSplit = hashLocation.split("#!/");
+          var hashName = hashSplit[1];
+          if(hashName !== '') {
+            var hash = window.location.hash;
+            if(hash === '') {
+              alert("你点击了返回键");
+            }
+          }
+        });
+        window.history.pushState('forward', null, './#forward');
+      }
+
     },
     mounted(){
 
