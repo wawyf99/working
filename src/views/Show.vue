36c6341371ee207d2436dd28b477e7c356a20b1e
张承 <star.cheung@live.cn>
2018-09-25 19:23:27 +0800

    2222

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -81,6 +81,20 @@
       this.go();
     },
     methods:{
+      shareBtn:function () {
+        let self = this;
+        var _url = window.location.href;
+        var u = navigator.userAgent;
+        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+        if (isiOS) {
+          _url = Global.appEntryUrl;
+        }
+        self.$http.get(global.url.wx_share, {
+          url : _url
+        }).then(res => {
+          self.wxShare(res);
+        });
+      },
       //点击模态框
       modalbox(){
         this.$refs.alertBox.style.display = 'block';
@@ -147,20 +161,6 @@
       },
       jumpFun(){
         window.location.href = 'http://www.baidu.com';
-      },
-      shareBtn:function () {
-        let self = this;
-        var _url = window.location.href;
-        var u = navigator.userAgent;
-        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
-        if (isiOS) {
-          _url = Global.appEntryUrl;
-        }
-        self.$http.get(global.url.wx_share, {
-          url : _url
-        }).then(res => {
-          self.wxShare(res);
-        });
       }
     },
     mounted(){
@@ -170,11 +170,10 @@
 
 
       var agent = navigator.userAgent.toLowerCase();
-      console.log(agent);
+      //console.log(agent);
       if (agent.match(/MicroMessenger/i) == "micromessenger") {
-        this.shareBtn();
+        self.shareBtn();
       }
-
 
       pushHistory();
 
