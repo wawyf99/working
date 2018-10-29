414c5a0af73af84de3461e85484835d7053414e1
张承 <star.cheung@live.cn>
2018-10-29 16:33:02 +0800

    s

--- src/utils/wxshare.js
+++ src/utils/wxshare.js
@@ -36,13 +36,13 @@ export default function wxShare ({title, desc, timelineTitle, link, imgUrl} = {}
         timestamp: res.data.timestamp,
         nonceStr: res.data.nonceStr,
         signature: res.data.signature,
-        jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData']
+        jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline']
       })
     });
   }
 
   Vue.wechat.ready(() => {
-    Vue.wechat.updateAppMessageShareData({
+    Vue.wechat.onMenuShareAppMessage({
       title: title, // 分享标题
       desc: desc || '这里是分享朋友的内容', // 分享描述
       link: link || window.location.href, // 分享链接
@@ -51,7 +51,7 @@ export default function wxShare ({title, desc, timelineTitle, link, imgUrl} = {}
 
       },
     })
-    Vue.wechat.updateTimelineShareData({
+    Vue.wechat.onMenuShareTimeline({
       title: timelineTitle || desc || '这里是分享朋友圈的内容', // 分享标题，由于分享到朋友圈没有desc，所以这里采用的策略是，指定的朋友圈分享标题优先，其次采用发送给朋友的描述，最后采用默认文案。
       link: link || window.location.href, // 分享链接
       imgUrl: imgUrl || 'https://dwz.cn/bQtHr9Iz', // 分享图标
