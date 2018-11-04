9e1709ecaf65df113b02a8e6115639d731035d47
张承 <star.cheung@live.cn>
2018-11-04 18:16:46 +0800

    s

--- src/utils/wxshare.js
+++ src/utils/wxshare.js
@@ -32,6 +32,7 @@ export default function wxShare ({title, desc, timelineTitle, link, logo, flock_
   }
 
   Vue.wechat.ready(() => {
+    console.log(title, desc, timelineTitle, link, logo, flock_logo);
     Vue.wechat.onMenuShareAppMessage({
       title: title, // 分享标题
       desc: desc || '这里是分享朋友的内容', // 分享描述
@@ -71,7 +72,7 @@ export default function wxShare ({title, desc, timelineTitle, link, logo, flock_
       },
     })
     Vue.wechat.onMenuShareTimeline({
-      title: timelineTitle || desc || '这里是分享朋友圈的内容', // 分享标题，由于分享到朋友圈没有desc，所以这里采用的策略是，指定的朋友圈分享标题优先，其次采用发送给朋友的描述，最后采用默认文案。
+      title: timelineTitle || '这里是分享朋友圈的内容', // 分享标题，由于分享到朋友圈没有desc，所以这里采用的策略是，指定的朋友圈分享标题优先，其次采用发送给朋友的描述，最后采用默认文案。
       link: link || window.location.href, // 分享链接
       imgUrl: flock_logo || 'https://dwz.cn/bQtHr9Iz', // 分享图标
       success: function () {
