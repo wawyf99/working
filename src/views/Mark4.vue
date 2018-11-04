9ab6022db3842e67ebbe64842b1f04304ebe208a
张承 <star.cheung@live.cn>
2018-11-04 18:32:34 +0800

    s

--- src/views/Mark4.vue
+++ src/views/Mark4.vue
@@ -99,7 +99,7 @@
           wxShare({ title: res.title, desc: res.describe, timelineTitle: res.flock_title, link: shareUrl , logo: res.logo, flock_logo: res.flock_logo });
         });*/
         self.$http.post(global.baseUrl+global.url.get_wx_share,{}).then(res => {
-          this.shareUrl = res.url;
+          self.shareUrl = res.url+this.wxid;
           var shareUrl = this.shareUrl+this.wxid,
             title = res.title,
             desc = res.describe,
@@ -107,7 +107,8 @@
             logo = res.logo,
             link = shareUrl,
             flock_logo = res.flock_logo;
-          wxShare({ title: '1', desc: '2', timelineTitle: '3', link: shareUrl , logo: '4' , flock_logo: '5'});
+          console.log(self.shareUrl);
+          wxShare({ title: '1', desc: '2', timelineTitle: '3', link: self.shareUrl , logo: '4' , flock_logo: '5'});
         });
 
       },
