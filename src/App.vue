e08f95df626afc1a0b285b2aae2bf1da039f5e08
张承 <star.cheung@live.cn>
2018-11-10 21:59:08 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" :style="uaSort">
     <router-view/>
   </div>
 </template>
 
 <script>
+  import ua from "./utils/userAgent";
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      uaSort: {
+        webkitOverflowScrolling: '',
+      },}
+    },
+  created(){
+    if(ua.versions.android){
+      this.uaSort.webkitOverflowScrolling = 'touch';
+    }
+  }
 }
 </script>
+
 
 <style lang="less" scoped="" type="text/css">
 
