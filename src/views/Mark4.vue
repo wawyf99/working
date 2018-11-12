25c93017d77039b5d0f60dcf2fa9c26c4207a798
张承 <star.cheung@live.cn>
2018-11-12 12:17:57 +0800

    s

--- src/views/Mark4.vue
+++ src/views/Mark4.vue
@@ -141,45 +141,6 @@
         window.history.pushState(state, "title", _url);
       }
 
-      window.document.oncontextmenu = function (e) {
-        e.preventDefault();
-      };
-
-      var startX = 0, startY = 0;
-
-      function touchStart(e) {
-        try {
-          var touch = e.touches[0],
-            x = Number(touch.pageX),
-            y = Number(touch.pageY);
-          startX = x;
-          startY = y;
-        } catch (e) {
-          alert(e);
-        }
-      }
-
-      document.addEventListener('touchstart', touchStart);
-      var ele = document.getElementById('join');
-
-      ele.ontouchmove = function (e) {
-        var point = e.touches[0],
-          eleTop = ele.scrollTop,
-          eleScrollHeight = ele.scrollHeight,
-          eleOffsetHeight = ele.offsetHeight,
-          eleTouchBottom = eleScrollHeight - eleOffsetHeight;
-        if (eleTop === 0) {
-          if (point.clientY > startY) {
-            e.preventDefault();
-          }
-        }
-        else if (eleTop === eleTouchBottom) {
-          if (point.clientY < startY) {
-            e.preventDefault()
-          }
-        }
-      };
-
     }
 
   }
