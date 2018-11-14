2ddc3e2fb1126381c28f4844fa8da81ecf9da4c7
张承 <star.cheung@live.cn>
2018-11-14 16:55:40 +0800

    s

--- src/views/Mark3.vue
+++ src/views/Mark3.vue
@@ -18,7 +18,7 @@
           <div class="ui-invite" v-cloak>{{invitor}} 邀您加入群聊</div>
           <input type="button" value="加入群聊" class="ui-btn" @click="show(2)">
           <div class="ui-role">
-            <p>1.您和群里其他人都不是朋友关系，请注意隐私安全。</p>
+            <p>1.您和群里其他人都不是朋友关系，注意隐私安全。</p>
             <p>2.该群聊人数较多，为减少新信息给您带来的打扰，建议进群后屏蔽消息通知。</p>
           </div>
         </div>
