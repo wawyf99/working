c20809bba804f406c20bb01b69cc44c095561229
张承 <star.cheung@live.cn>
2018-11-14 16:57:06 +0800

    s

--- src/views/Mark4.vue
+++ src/views/Mark4.vue
@@ -4,7 +4,7 @@
       <div id="scroller">
         <div style="height: 0.5rem;"></div>
         <div class="ui-show-step1 step" ref="go1">{{now}}</div>
-        <div class="ui-show-step2 step" ref="go2">"<span>{{invitor}}</span>"邀请你加入群聊，群聊参与人还有：辞予、那一夜、床摇得厉害、你的呻吟、甜腻、强哥、七尺大乳、漂洋过海、用贞操换真钞、清晨的眼泪、孟老师、性感↗小娘们、孤寂、淫领风骚、小影、爱到深处て腿自开、无心、吻我杀我、林萌</div>
+        <div class="ui-show-step2 step" ref="go2">"<span>{{invitor}}</span>"邀您加入群聊，群聊参与人还有：辞予、那一夜、床摇得厉害、你的呻吟、甜腻、强哥、七尺大乳、漂洋过海、用贞操换真钞、清晨的眼泪、孟老师、性感↗小娘们、孤寂、淫领风骚、小影、爱到深处て腿自开、无心、吻我杀我、林萌</div>
         <div class="ui-show-step3 step clearfix" ref="go3">
           <div class="show-left" :style="background4"></div>
           <div class="show-right">
